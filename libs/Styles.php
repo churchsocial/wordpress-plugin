0e494607e5cb0a3f7f63c1effa9b487d31e75a15
Jonathan Reinink <jonathan@reinink.ca>
2020-05-06 13:12:01 -0400

    Clean up plugin styles

--- libs/Styles.php
+++ libs/Styles.php
@@ -4,33 +4,13 @@ namespace ChurchSocial;
 
 class Styles
 {
-    protected $plugin_file;
-
     public function __construct($plugin_file)
     {
-        $this->plugin_file = $plugin_file;
-        $this->theme = get_option('church_social_theme');
+        $theme = get_option('church_social_theme');
 
-        if (is_admin()) {
-            $this->loadAdminStyles();
-        } else {
-            $this->loadThemeStyles();
-        }
-    }
-
-    protected function loadAdminStyles()
-    {
-        add_filter('admin_enqueue_scripts', function () {
-            wp_register_style('church_social', plugins_url('/css/admin.css', $this->plugin_file));
-            wp_enqueue_style('church_social');
-        });
-    }
-
-    protected function loadThemeStyles()
-    {
-        if ($this->theme) {
-            add_filter('wp_enqueue_scripts', function () {
-                wp_register_style('church_social', plugins_url('/css/'.$this->theme.'.css', $this->plugin_file));
+        if (!is_admin() && $theme) {
+            add_filter('wp_enqueue_scripts', function () use ($theme, $plugin_file) {
+                wp_register_style('church_social', plugins_url('/css/'.$theme.'.css', $plugin_file));
                 wp_enqueue_style('church_social');
             });
         }
--- readme.txt
+++ readme.txt
@@ -35,6 +35,10 @@ The purpose of this plugin is to show content from within a customer's Church So
 
 == Changelog ==
 
+= 1.5.1 =
+
+* Clean up plugin styles
+
 = 1.5.0 =
 
 * Add the ability to add a copyright notice on the sermon page
